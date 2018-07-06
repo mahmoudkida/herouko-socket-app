9f565d4aa19435cb6c01226e0ede893f7a7769c0
mahmoudkida <mahmoudkida@gmail.com>
2018-07-06 14:25:50 +0300

    update static files locations

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+app.use(express.static(path.join(__dirname, 'public')));
 const io = socketIO(server);
 
 io.on('connection', (socket) => {
