ac72aa539bfdc9b0cd3e69d38715386f01ba20a2
mahmoudkida <mahmoudkida@gmail.com>
2018-07-06 14:21:00 +0300

    add uxawy bot

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
+const apiai = require('apiai')('bccb47faa1144397aad612f4acf8f88e');
+
 
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
@@ -15,7 +17,28 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   console.log('Client connected');
+    socket.on('chat message', (text) => {
+
+        // Get a reply from API.AI
+        console.log("client text: " + text);
+
+        let apiaiReq = apiai.textRequest(text, {
+            sessionId: Math.random() * 1000
+        });
+
+        apiaiReq.on('response', (response) => {
+            let aiText = response.result.fulfillment.speech;
+            console.log("bot text: " + aiText);
+            socket.emit('bot reply', aiText); // Send the result back to the browser!
+        });
+
+        apiaiReq.on('error', (error) => {
+            console.log(error);
+        });
+
+        apiaiReq.end();
+
+    });
   socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
